b21eca46ae7276d3184c6259d11800f28ca3243a
McCreepyyy <mccreepyy@hotmail.com>
2024-05-21 14:30:30 +1200

    Flask Framework
    - Added TXT file for Project Submission
    - Added full language options and sidebar to lessonhub.html
    - Started flask framework on characters.html

--- Duolingo Flask Project/app.py
+++ Duolingo Flask Project/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 import os
 
 app = Flask(__name__)
@@ -11,6 +12,10 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    hearts = db.Column(db.Integer, default=5)
+    gems = db.Column(db.Integer, default=0)
+    day_streak = db.Column(db.Integer, default=0)
+    last_login = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -31,11 +36,6 @@ def getstarted():
 def privacypolicy():
     return render_template('privacypolicy.html')
 
-# Remove the duplicate index route
-# @app.route('/index')
-# def index():
-#     return render_template('index.html')
-
 @app.route('/indexjp')
 def indexjp():
     return render_template('indexjp.html')
@@ -43,6 +43,45 @@ def indexjp():
 @app.route('/indexfr')
 def indexfr():
     return render_template('indexfr.html')
+
+@app.route('/characters')
+def characters():
+    return render_template('characters.html')
+
+@app.route('/leaderboards')
+def leaderboards():
+    return render_template('leaderboards.html')
+
+@app.route('/quests')
+def quests():
+    return render_template('quests.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/lessonhubjp')
+def lessonhubjp():
+    return render_template('lessonhubjp.html')
+
+@app.route('/lessonhubfr')
+def lessonhubfr():
+    return render_template('lessonhubfr.html')
+
+@app.route('/lessonhub')
+def lessonhub():
+    # Fetch the user from the database. This is just an example.
+    # You'll need to fetch the actual user based on your application's logic.
+    user = User.query.first()
+    return render_template('lessonhub.html', user=user)
 
 @app.route('/submit_form', methods=['POST'])
 def submit_form():
@@ -62,13 +101,27 @@ def login():
     password = request.form['password']
     user = User.query.filter_by(email=email).first()
     if user and user.password == password:
-        return redirect(url_for('index'))
+        # Update hearts
+        hours_since_last_login = (datetime.utcnow() - user.last_login).total_seconds() / 3600
+        new_hearts = min(5, user.hearts + int(hours_since_last_login / 4))
+        user.hearts = new_hearts
+
+        # Update day streak
+        if user.last_login < datetime.utcnow() - timedelta(days=1):
+            user.day_streak += 1
+
+        # Update last login
+        user.last_login = datetime.utcnow()
+
+        db.session.commit()
+
+        return redirect(url_for('lessonhub'))  # Redirect to lessonhub after login
     else:
         return 'Invalid email or password'
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  # Create the table again
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
